dabd2c56bce9d01b2d8ac993edc45eb8743cafac
newmanrs <newmanrs@umich.edu>
2021-10-14 21:03:42 -0700

    Support for constants, and wrapping use of stack.pop() in the rpn function with take_n generator with helpful error handling

--- rpncalc.py
+++ rpncalc.py
@@ -1,6 +1,35 @@
 import sys
+import copy
 import math
 from enum import Enum, unique
+
+
+class Constant:
+
+    constants = {
+        'pi'  : math.pi,
+        'tau' : 2*math.pi,
+        'c'   : 299792458,
+        'h'   : 6.62607015e-34,
+        'mu0' : 1.2566370621219e-6,
+        'Na'  : 6.02214076e23,
+        'kb'  : 1.380649e-23,
+        'R'   : 8.314462618,
+        'G'   : 6.6743015e-11,
+        'g'   : 9.80665,
+        'me'  : 9.109383701528e-31,
+        'mp'  : 1.6726219236951e-27,
+        'mn'  : 1.6749274980495e-27,
+    }
+
+    @classmethod
+    def __call__(cls,arg):
+        try:
+            return cls.constants[arg]
+        except KeyError:
+            # Convert KeyError to ValueError to make this behave
+            # like the enums below during argument parsing
+            raise ValueError
 
 @unique
 class BinaryOperator(Enum):
@@ -9,9 +38,8 @@ class BinaryOperator(Enum):
     multiplication = '*'
     division = '/'
     integerdivision = '//'
-    atan2 = 'atan2'
-    log_base = 'log_base'
-
+    atan2 = 'atan2'        # atan2(y, x) input 1000 1 atan2 for ~pi/2
+    log_base = 'log_base'  # 100 10 log_base gives 2
 
 @unique
 class UnaryOperator(Enum):
@@ -23,22 +51,25 @@ class UnaryOperator(Enum):
     atan = 'atan'
     to_int = 'int'
     exp = 'exp'
-    expmxsq = 'expmxsq'
+    expmxsq = 'expmxsq'    # exp(-x^2)
     ln = 'ln'
     sqrt = 'sqrt'
 
 @unique
-class NonaryOperator(Enum):
-    stack = 'stack'
+class IdempotentOperator(Enum):
+    print_stack = 'print'
 
 
 def parse_args(args):
-
+    """
+    Convert the initial list of argv[1:] to numeric values or appropriate actions
+    or operators for the calculator reverse polish evaluation loop.
+    """
     parsedargs = []
 
     for arg in args:
         parsed = False
-        for t in [int, float, BinaryOperator, UnaryOperator, NonaryOperator]:
+        for t in [int, float, Constant(), BinaryOperator, UnaryOperator, IdempotentOperator]:
             try:
                 parsedargs.append(t(arg))
                 parsed = True
@@ -47,14 +78,24 @@ def parse_args(args):
             else:
                 break
         if not parsed:
-            msg = f"Unknown operation parsing arg {arg}"
+            msg = f"Unknown operation parsing arg '{arg}'"
             raise ValueError(msg)
+
     return parsedargs
+
+def take_n(n,stack, op):
+    debug = copy.copy(stack)
+    for i in range(n):
+        try:
+            yield stack.pop()
+        except IndexError as e:
+            msg = f"Empty stack processing {op} trying to pop {n} values. Stack: {debug}"
+            raise IndexError(msg) from e
 
 def binary_operator(op, stack):
 
-    v1 = stack.pop()
-    v0 = stack.pop()
+    v1, v0 = tuple(take_n(2,stack,op))
+
     match op:
 
         case BinaryOperator.addition:
@@ -79,8 +120,10 @@ def binary_operator(op, stack):
 
 def unary_operator(op, stack):
 
-    x = stack.pop()
+    x = tuple(take_n(1,stack,op))
+
     match op:
+
         case UnaryOperator.sin:
             r = math.sin(x)
         case UnaryOperator.cos:
@@ -108,10 +151,10 @@ def unary_operator(op, stack):
             raise NotImplementedError(msg)
     stack.append(r)
 
-def nonary_operator(action, stack):
+def idempotent_operator(action, stack):
 
     match action:
-        case NonaryOperator.stack:
+        case IdempotentOperator.print_stack:
             print(f"Stack: {stack}")
         case _:
             msg = f"Missing case match for action {op}"
@@ -124,24 +167,28 @@ def rpn(args):
         match arg:
             case _ if isinstance(arg, (int | float)):
                 stack.append(arg)
+            case _ if isinstance(arg, Constant):
+                stack.append(arg)
             case _ if isinstance(arg, BinaryOperator):
                 binary_operator(arg,stack)
             case _ if isinstance(arg, UnaryOperator):
                 unary_operator(arg,stack)
-            case _ if isinstance(arg, NonaryOperator):
-                nonary_operator(arg, stack)
+            case _ if isinstance(arg, IdempotentOperator):
+                idempotent_operator(arg, stack)
     return stack
 
 def main():
     """
     RPN Calculator.  Entry point to script installed by setup.py.
     """
-    if len(sys.argv) > 2:
+
+    if len(sys.argv) > 2: # Parse as given
         parsedargs = parse_args(sys.argv[1:])
-    if len(sys.argv) == 2:
+    elif len(sys.argv) == 2: # Args encased in string
         parsedargs = parse_args(sys.argv[1].split())
     stack = rpn(parsedargs)
 
+    #If only one item on stack, print value, otherwise print stack
     if len(stack) == 1:
         print(stack[0])
     else:
