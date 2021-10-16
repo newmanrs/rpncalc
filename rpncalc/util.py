4a36017ff4ab5c742a83784c5439f3ecfae81042
newmanrs <newmanrs@umich.edu>
2021-10-15 20:23:32 -0700

    util.py commentsfor take_n

--- rpncalc/util.py
+++ rpncalc/util.py
@@ -1,6 +1,10 @@
-def take_n(n,stack, op):
+def take_n(n,stack, action = None):
+    """
+    Generator that pops up to n values from stack.
+    Optional action gives clearer error messages.
+    """
     if n > len(stack):
-        msg = f"Empty stack processing {op} trying to pop {n} values. Stack: {stack}"
+        msg = f"Empty stack processing {action} trying to pop {n} values. Stack: {stack}"
         raise IndexError(msg)
 
     for i in range(n):
